043c080937982b5142af4af574182d0587054335
Charliegs <35603121+Charliegs@users.noreply.github.com>
2019-01-04 12:55:50 +0100

    Dash guay

--- Assets/ALPHA/SCRIPTS/Dash.cs
+++ Assets/ALPHA/SCRIPTS/Dash.cs
@@ -6,12 +6,22 @@ public class Dash : MonoBehaviour
 {
     public float pushPower = 0.05f;
     public CharacterController characterController;
+    public Transform indicador;
+    Vector3 forwardVector;
+    GameObject cam;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera"); 
+    }
 
     void Update()
     {
+        forwardVector = cam.transform.forward;
+
         if (Input.GetKeyUp(KeyCode.Q))
         {
-            characterController.Move(this.transform.forward * pushPower);
-        }
+            characterController.Move(forwardVector * pushPower);
+        }       
     }
 }
