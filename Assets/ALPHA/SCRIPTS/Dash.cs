b7a32b79c760b24df9a4ea4e2994e1c08a8c0008
Charliegs <35603121+Charliegs@users.noreply.github.com>
2019-01-04 13:12:23 +0100

    Dash con cosa visual para saber donde aterrizar mas o menos muy cutre

--- Assets/ALPHA/SCRIPTS/Dash.cs
+++ Assets/ALPHA/SCRIPTS/Dash.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    public GameObject indicador;
     public float pushPower = 0.05f;
     public CharacterController characterController;
-    public Transform indicador;
     Vector3 forwardVector;
     GameObject cam;
 
     private void Start()
     {
-        cam = GameObject.Find("Main Camera"); 
+        cam = GameObject.Find("Main Camera");
     }
 
     void Update()
     {
         forwardVector = cam.transform.forward;
 
+        if (Input.GetKey(KeyCode.Q))
+        {
+            indicador.SetActive(true);
+        }
+        else
+        {
+            indicador.SetActive(false);
+        }
+
         if (Input.GetKeyUp(KeyCode.Q))
         {
             characterController.Move(forwardVector * pushPower);
-        }       
+            indicador.SetActive(false);
+        }
     }
 }
