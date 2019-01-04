886098507df4dcf3e8588e1d2d592224644d114c
Charliegs <35603121+Charliegs@users.noreply.github.com>
2019-01-04 12:37:27 +0100

    Dash (version cutre) (ahora lo intento mejorar)

---
+++ Assets/ALPHA/SCRIPTS/Dash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    public float pushPower = 0.05f;
+    public CharacterController characterController;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            characterController.Move(this.transform.forward * pushPower);
+        }
+    }
+}
