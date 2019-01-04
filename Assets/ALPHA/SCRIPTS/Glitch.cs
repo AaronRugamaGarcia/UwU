805aefac9a47e1f2a4337a321794b35ce4ccc48a
Charliegs <35603121+Charliegs@users.noreply.github.com>
2019-01-04 15:26:09 +0100

    Raycast para glitch (aun no hace nada, pusheo por si acaso)

---
+++ Assets/ALPHA/SCRIPTS/Glitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glitch : MonoBehaviour
+{
+    Ray rayo;
+    RaycastHit hit;
+    GameObject cam;
+    Vector3 forwardVector;
+
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+        Vector3 forwardVector = cam.transform.forward * 8;
+        rayo = new Ray(cam.transform.position, forwardVector);
+        Debug.DrawRay(cam.transform.position, forwardVector, Color.green);
+    }
+}
