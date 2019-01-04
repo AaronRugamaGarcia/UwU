f908255dca7614d7cd2708f1da236be03b687a71
AaronRugamaGarcia <35967309+AaronRugamaGarcia@users.noreply.github.com>
2019-01-04 13:37:20 +0100

    SequenceManager_00

---
+++ Assets/ALPHA/SCRIPTS/SequenceManager.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerPersistent : MonoBehaviour
+{
+    public Scene Sc_Scene0;
+    public string s_Scene0 = "Level0";
+    public bool b_Scene0;
+
+    public Scene Sc_Scene1;
+    public string s_Scene1 = "TreeHouse";
+    public bool b_Scene1;
+
+    public Scene Sc_Scene2;
+    public string s_Scene2 = "Tokyo";
+    public bool b_Scene2;
+
+    public Scene Sc_Scene3;
+    public string s_Scene3 = "Oficine";
+    public bool b_Scene3;
+
+
+
+    //START::::::::::::::::::::::::::::::::::
+    private void Start()
+    {
+        b_Scene0 = true;
+        b_Scene1 = false;
+        b_Scene2 = false;
+        b_Scene3 = false;
+        StartCoroutine(LoadSceneAndSetActive0());
+
+
+
+        StartCoroutine(UnloadSceneAndSetActive1());
+        StartCoroutine(UnloadSceneAndSetActive2());
+        StartCoroutine(UnloadSceneAndSetActive3());
+    }
+
+
+    //INPUTS::::::::::::::::::::::::::::::::::::::
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !b_Scene0)
+        {
+
+            b_Scene0 = true;
+            b_Scene1 = false;
+            b_Scene2 = false;
+            b_Scene3 = false;
+            StartCoroutine(LoadSceneAndSetActive0());
+
+
+
+            StartCoroutine(UnloadSceneAndSetActive1());
+            StartCoroutine(UnloadSceneAndSetActive2());
+            StartCoroutine(UnloadSceneAndSetActive3());
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && !b_Scene1)
+        {
+
+            b_Scene0 = false;
+            b_Scene1 = true;
+            b_Scene2 = false;
+            b_Scene3 = false;
+            StartCoroutine(LoadSceneAndSetActive1());
+
+
+
+
+            StartCoroutine(UnloadSceneAndSetActive0());
+            StartCoroutine(UnloadSceneAndSetActive2());
+            StartCoroutine(UnloadSceneAndSetActive3());
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && !b_Scene2)
+        {
+
+            b_Scene0 = false;
+            b_Scene1 = false;
+            b_Scene2 = true;
+            b_Scene3 = false;
+            StartCoroutine(LoadSceneAndSetActive2());
+
+
+
+            StartCoroutine(UnloadSceneAndSetActive0());
+            StartCoroutine(UnloadSceneAndSetActive1());
+            StartCoroutine(UnloadSceneAndSetActive3());
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && !b_Scene3)
+        {
+
+            b_Scene0 = false;
+            b_Scene1 = false;
+            b_Scene2 = false;
+            b_Scene3 = true;
+            StartCoroutine(LoadSceneAndSetActive3());
+
+
+
+            StartCoroutine(UnloadSceneAndSetActive0());
+            StartCoroutine(UnloadSceneAndSetActive2());
+            StartCoroutine(UnloadSceneAndSetActive1());
+
+        }
+
+    }
+
+
+
+
+
+    //LOAD SCENES:::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    IEnumerator LoadSceneAndSetActive0()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(("Level0"), LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator LoadSceneAndSetActive1()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(("TreeHouse"), LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator LoadSceneAndSetActive2()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(("Tokyo"), LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator LoadSceneAndSetActive3()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(("Oficine"), LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+
+
+
+
+    //UNLOAD SCENES:::::::::::::::::::::::::::::::::::::::::::::: 
+    IEnumerator UnloadSceneAndSetActive0()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync("Level0");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator UnloadSceneAndSetActive1()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync("TreeHouse");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator UnloadSceneAndSetActive2()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync("Tokyo");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator UnloadSceneAndSetActive3()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync("Officine");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
