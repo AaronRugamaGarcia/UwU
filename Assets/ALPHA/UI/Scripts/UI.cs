85a82352b46777fda0747e10ef640ad389b099c7
Charliegs <35603121+Charliegs@users.noreply.github.com>
2019-01-04 11:43:17 +0100

    UI arreglada

--- Assets/ALPHA/UI/Scripts/UI.cs
+++ Assets/ALPHA/UI/Scripts/UI.cs
@@ -6,30 +6,23 @@ public class UI : MonoBehaviour
 {
     public GameObject canvas;
 
-
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         if (Input.GetKey(KeyCode.Tab))
         {
             canvas.SetActive(true);
             Time.timeScale = 0.3f;
-            //GameObject.Find("Main Camera").GetComponent<PlayerLook>().enabled = false;
-            //GameObject.Find("Player").GetComponent<PlayerMove>().enabled = false;
-            //Cursor.lockState = CursorLockMode.Locked;
-            Cursor.lockState = CursorLockMode.None;
+            GameObject.Find("Main Camera").GetComponent<vp_FPCamera>().enabled = false;
+            GameObject.Find("Player").GetComponent<vp_FPController>().enabled = false;
             Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
         else if (!Input.GetKey(KeyCode.Tab))
         {
             canvas.SetActive(false);
             Time.timeScale = 1;
-            //GameObject.Find("Main Camera").GetComponent<PlayerLook>().enabled = true;
-            //GameObject.Find("Player").GetComponent<PlayerMove>().enabled = true;
+            GameObject.Find("Main Camera").GetComponent<vp_FPCamera>().enabled = true;
+            GameObject.Find("Player").GetComponent<vp_FPController>().enabled = true;
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
