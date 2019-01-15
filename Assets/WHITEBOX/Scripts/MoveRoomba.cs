35dcaabbdcffed79cb0c4e1aad3e4802c4bd1299
Charliegs <35603121+Charliegs@users.noreply.github.com>
2019-01-15 21:08:43 +0100

    APARTAMENTOS CON COLLIDERS Y ROOMBA ACABADA

--- Assets/WHITEBOX/Scripts/MoveRoomba.cs
+++ Assets/WHITEBOX/Scripts/MoveRoomba.cs
@@ -5,39 +5,42 @@ using UnityEngine.AI;
 
 public class MoveRoomba : MonoBehaviour
 {
-    public float timer;
-    public float newtarget;
-    public float speed;
-    public NavMeshAgent agent;
-    public Vector3 target;
 
-    void Start()
+    public float radio;
+    public float tiempoMovimiento;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+
+    void OnEnable()
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
+        agent = this.GetComponent<NavMeshAgent>();
+        timer = tiempoMovimiento;
     }
 
     void Update()
     {
         timer += Time.deltaTime;
-        agent.speed = speed;
 
-        if (timer >= newtarget)
+        if (timer >= tiempoMovimiento)
         {
-            newTarget();
+            Vector3 newPos = RandomNavSphere(transform.position, radio, -1);
+            agent.SetDestination(newPos);
             timer = 0;
         }
     }
 
-    void newTarget()
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
     {
-        float myX = gameObject.transform.position.x;
-        float myZ = gameObject.transform.position.z;
+        Vector3 randDirection = Random.insideUnitSphere * dist;
 
-        float xPos = myX + Random.Range(myX - 100, myX + 100);
-        float zPos = myZ + Random.Range(myZ - 100, myZ + 100);
+        randDirection += origin;
 
-        target = new Vector3(xPos, gameObject.transform.position.y, zPos);
+        NavMeshHit navHit;
 
-        agent.SetDestination(target);
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
     }
 }
