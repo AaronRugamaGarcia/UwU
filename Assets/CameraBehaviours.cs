b802eee4a78b372d173e263d920c9eba04b5e037
AaronRugamaGarcia <35967309+AaronRugamaGarcia@users.noreply.github.com>
2019-01-31 20:57:04 +0100

    Puzzle_Servers_00

--- Assets/CameraBehaviours.cs
+++ Assets/CameraBehaviours.cs
@@ -16,7 +16,7 @@ public class CameraBehaviours : MonoBehaviour
     public bool b_room;
 
     public Animator anim;
-    public string animationName;
+    
 
     // Start is called before the first frame update
     void Start()
