2c85720b5d0c3b0b5e4393c815e3dbeaf1af38d7
danisoriarev <35929204+danisoriarev@users.noreply.github.com>
2019-03-08 17:05:00 +0100

    Shader DataMosh

---
+++ Assets/IA/Scenes IA/pruebasshadermosh/DatamoshEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+
+public class DatamoshEffect : MonoBehaviour
+{
+
+    public Material DMmat; //datamosh material
+
+    void Start()
+    {
+        this.GetComponent<Camera>().depthTextureMode = DepthTextureMode.MotionVectors;
+        //generate the motion vector texture @ '_CameraMotionVectorsTexture'
+    }
+    private void Update()
+    {
+        Shader.SetGlobalInt("_Button", Input.GetButton("Fire1") ? 1 : 0);
+        // question mark operator allows us to use a bool as an integer
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, DMmat);
+    }
+}
