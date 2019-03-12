5b2fcbf5bdcb96eda2db4aec0d3a13d9c33ec2af
AaronRugamaGarcia <35967309+AaronRugamaGarcia@users.noreply.github.com>
2019-03-12 12:35:53 +0100

    Puzzle Buzones

--- Assets/ALPHA/SCRIPTS/puzzleBuzones.cs
+++ Assets/ALPHA/SCRIPTS/puzzleBuzones.cs
@@ -7,6 +7,9 @@ public class puzzleBuzones : MonoBehaviour
     Ray rayo;
     RaycastHit hit;
     public GameObject cam;
+    public GameObject direccion1; public GameObject luz1; public GameObject mail1;
+    public GameObject direccion2; public GameObject luz2; public GameObject mail2;
+    public GameObject direccion3; public GameObject luz3; public GameObject mail3;
 
     private void Awake()
     {
@@ -15,7 +18,7 @@ public class puzzleBuzones : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -24,9 +27,48 @@ public class puzzleBuzones : MonoBehaviour
         rayo = new Ray(cam.transform.position, forwardVector2);
         Debug.DrawRay(cam.transform.position, forwardVector2, Color.red);
 
-        if (Physics.Raycast(rayo, out hit))
+        if (Physics.Raycast(rayo, out hit) && hit.transform.gameObject.tag == "Mail")
         {
-            Debug.Log(hit);
+            if (Input.GetKey(KeyCode.E) && hit.transform.gameObject.name == "Mail1")
+            {
+                direccion1.SetActive(true); luz1.SetActive(true);
+                direccion2.SetActive(false); luz2.SetActive(false);
+                direccion3.SetActive(false); luz3.SetActive(false);
+            }
+            else if (Input.GetKey(KeyCode.E) && hit.transform.gameObject.name == "Mail2")
+            {
+                direccion1.SetActive(false); luz1.SetActive(false);
+                direccion2.SetActive(true); luz2.SetActive(true);
+                direccion3.SetActive(false); luz3.SetActive(false);
+            }
+            else if (Input.GetKey(KeyCode.E) && hit.transform.gameObject.name == "Mail3")
+            {
+                direccion1.SetActive(false); luz1.SetActive(false);
+                direccion2.SetActive(false); luz2.SetActive(false);
+                direccion3.SetActive(true); luz3.SetActive(true);
+            }
+        }
+
+        if (Physics.Raycast(rayo, out hit) && hit.transform.gameObject.tag == "MailLuz")
+        {
+            if (Input.GetKey(KeyCode.E) && hit.transform.gameObject.name == "MailLuz1")
+            {
+                direccion1.SetActive(false);
+                luz1.SetActive(false);
+                mail1.SetActive(false);
+            }
+            else if (Input.GetKey(KeyCode.E) && hit.transform.gameObject.name == "MailLuz2")
+            {
+                direccion2.SetActive(false);
+                luz2.SetActive(false);
+                mail2.SetActive(false);
+            }
+            else if (Input.GetKey(KeyCode.E) && hit.transform.gameObject.name == "MailLuz3")
+            {
+                direccion3.SetActive(false);
+                luz3.SetActive(false);
+                mail3.SetActive(false);
+            }
         }
     }
 }
