9b77f7315e8ebdb5bea22698a5fc6b7eea2efebd
danisoriarev <35929204+danisoriarev@users.noreply.github.com>
2019-03-12 11:43:00 +0100

    afa

---
+++ Assets/ALPHA/SCRIPTS/puzzleBuzones.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzleBuzones : MonoBehaviour
+{
+    Ray rayo;
+    RaycastHit hit;
+    public GameObject cam;
+
+    private void Awake()
+    {
+        cam = GameObject.Find("Main Camera");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 forwardVector2 = cam.transform.forward * 5;
+        rayo = new Ray(cam.transform.position, forwardVector2);
+        Debug.DrawRay(cam.transform.position, forwardVector2, Color.red);
+
+        if (Physics.Raycast(rayo, out hit))
+        {
+            Debug.Log(hit);
+        }
+    }
+}
