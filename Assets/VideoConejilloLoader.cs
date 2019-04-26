6edc4e4b2be2384de5bc57ef798855bb60598e1f
danisoriarev <35929204+danisoriarev@users.noreply.github.com>
2019-04-26 19:53:09 +0200

    DEDEEEEEE!_2604

---
+++ Assets/VideoConejilloLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoConejilloLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Loader");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Loader()
+    {
+
+        yield return new WaitForSeconds(32.0f);
+        SceneManager.LoadScene(5);
+        yield return null;
+    }
+}
