65d3b06b2991fe2b0f7e1c67c5bd21a9f8d31aba
danisoriarev <35929204+danisoriarev@users.noreply.github.com>
2019-04-29 12:35:19 +0200

    Datamosh

---
+++ Assets/IA/Scenes IA/Nivel final/emisivoCubos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emisivoCubos : MonoBehaviour
+{
+    Material mater;
+    Color randomColor;
+    MeshRenderer mr;
+    void Start()
+    {
+        mr = gameObject.GetComponent<MeshRenderer>();
+        mater = new Material(Shader.Find("Standard"));
+        mr.material = mater;
+        randomColor = new Color(Random.value, Random.value, Random.value);
+    }
+
+    void Update()
+    {
+        mater.EnableKeyword("_EMISSION");
+        mater.SetColor("_EmissionColor", randomColor);
+
+    }
+}
